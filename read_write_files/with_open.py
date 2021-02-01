16471365bc6a0a688ead6e11d5a5b24c9c317146
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-02-01 19:11:03 +0700

    added file_ex.txt and with_open.py additional code

--- read_write_files/with_open.py
+++ read_write_files/with_open.py
@@ -2,3 +2,12 @@ with open('file_ex.txt', 'r') as file:
     contents = file.read()
 
 print(contents)
+
+# we use this methid when we want to read the contents of a file into a single string
+# or when we want to specify how many characterrs to read
+with open('file_ex.txt', 'r') as example_file:
+    first_ten_chars = example_file.read(10)
+    the_rest = example_file.read()
+
+print("The first 10 characters:", first_ten_chars)
+print("The rest of the file:", the_rest)
