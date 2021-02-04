f20f1a49e729c7a0cff89f01bd4c50829fa8bc32
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-02-04 21:20:05 +0700

    added mapping dic

---
+++ dictionaries/mapping_value_to_key.py
@@ -0,0 +1,12 @@
+table = {'Holly Grail':         '1975',
+        "Life of Brian":        '1979',
+        'The Meaning of Life':  '1983'}
+
+print(table['Holly Grail'])
+
+print(list(table.items()))
+
+# dictionary usage notes
+#1 sequence operations don't work
+#2 assinging new indexes add entries
+#3 keys need not always be strings
