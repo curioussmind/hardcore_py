c5f43a629faf0c556c818e0b6c32e1c38a2d32c5
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-02-04 20:49:17 +0700

    added some additional lists info

--- lists/list_comprehensions.py
+++ lists/list_comprehensions.py
@@ -22,3 +22,35 @@ print(matrix[1]) # [4, 5, 6]
 print(matrix[1][1]) # 5
 print(matrix[2][0])
 print(matrix)
+
+# changing list
+# keep in mind that unlike string lists are mutable, meaning we can overwrite its former value without making a new list
+li = ['spam', 'Spam', 'SPAM']
+li[1] = 'eggs'  # index assignment
+print(li) # ['spam', 'eggs', 'SPAM']
+
+li[0:2] = ['eat', 'more'] # slice assignment: delete + insert
+print(li) # ['eat', 'more', 'SPAM!]
+
+# list method calls
+lis = ['eat', 'more', 'SPAM!']
+lis.append('please') # append method call: add item at end
+print(lis)
+lis.sort() # sort item
+print(lis)
+
+# other common list methods
+liss = [1, 2]
+liss.extend([3, 4, 5]) # add many items at the end
+print(liss)
+
+liss.pop() #d delete and return the last item
+liss.reverse() # In-placed reversal method
+
+my_food = ['spam', 'eggs', 'ham']
+my_food.index('eggs') # index of an object (search/find)
+my_food.insert(1, 'toast') # insert at position
+my_food.reverse('ham') #delete by value
+my_food.pop(1) # delete by position
+my_food.count('eggs') # number of occurences
+del my_food[1:] # delete an entire section
