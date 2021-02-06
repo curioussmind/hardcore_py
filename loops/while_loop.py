cf53902a81b417bec0dd43394f608f72e7fd8b55
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-02-06 20:16:55 +0700

    more on while loops

--- loops/while_loop.py
+++ loops/while_loop.py
@@ -1,5 +1,27 @@
+# while loops executes block of statements as long as a test at the toop keeps evaluating to a true value
+# if the test is false, the body never runs and thenn
+
+# GENERAL FORMAT
+#while test:            # Loop test
+#    statements         # Loop body
+#else:                  # Optimal else
+#    statements         # Run if didn't exit loop with break
+
+x = 'spam'
+while x:                # whule x is not empty
+    print(x, end=' ')    
+    x = x[1:]           # strip the first char off x
+# expected output >>> spam pam am m
+
+a = 0; b = 10
+while a < b:
+    print(a, end=' ')
+    a += 1  # equal to a = a + 1
+# expected output >>> 0 1 2 3 4 5 6 7 8 9
+
+
 rabbits = 3
 while rabbits > 0:
     print(rabbits)
     rabbits = rabbits - 1
-    
+
